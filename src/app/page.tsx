4ada49e2838cf966bec835befab9c7f86546dd3d
awais512 <raza.awais25@gmail.com>
2024-11-15 23:08:39 +0000

    Auth UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { AuthScreen } from "@/features/auth/components/auth-screen";
 
 export default function Home() {
-  return <Button>Hello</Button>;
+  return <AuthScreen />;
 }
