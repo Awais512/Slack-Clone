ef8ae060e7158930d36f9a839639b20883e2dad7
awais512 <raza.awais25@gmail.com>
2024-11-19 14:21:09 +0000

    added jotai and create workspace modal

---
+++ src/features/workspaces/components/create-workspace-modal.tsx
@@ -0,0 +1,42 @@
+"use client";
+import {
+  Dialog,
+  DialogHeader,
+  DialogDescription,
+  DialogTitle,
+  DialogContent,
+} from "@/components/ui/dialog";
+import { useCreateWorkspaceModal } from "../store/use-create-workspace-modal";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+export const CreateWorkspaceModal = () => {
+  const [open, setOpen] = useCreateWorkspaceModal();
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={handleClose}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Add a workspace</DialogTitle>
+        </DialogHeader>
+        <form className="space-y-4">
+          <Input
+            value=""
+            disabled={false}
+            required
+            autoFocus
+            min={3}
+            placeholder="Workspace Name e.g 'work', 'personal', 'home'"
+          />
+          <div className="flex justify-end">
+            <Button disabled={false}>Create</Button>
+          </div>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+};
