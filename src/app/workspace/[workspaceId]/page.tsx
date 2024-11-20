ba56d7f8b82e0cacc83d77220647b83876a0fff2
awais512 <raza.awais25@gmail.com>
2024-11-20 15:12:46 +0000

    Workspace switcher

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,16 +1,5 @@
-"use client";
-
-import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
-import { useWorkspaceId } from "@/hooks/use-workspace-id";
-
 const WorkspaceIdPage = () => {
-  const workspaceId = useWorkspaceId();
-  const { data } = useGetWorkspace({ id: workspaceId });
-  return (
-    <div>
-      ID {workspaceId} {JSON.stringify(data)}
-    </div>
-  );
+  return <div>workspace ID</div>;
 };
 
 export default WorkspaceIdPage;
