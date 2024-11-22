f23c0767ca3fa09fd8d9d0f38e423383dbe0e255
awais512 <raza.awais25@gmail.com>
2024-11-22 11:04:11 +0000

    Workspace Sidebar

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -2,6 +2,12 @@
 import React from "react";
 import { Toolbar } from "./toolbar";
 import { Sidebar } from "./sidebar";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import { WorkspaceSidebar } from "./workspace-sidebar";
 
 interface Props {
   children: React.ReactNode;
@@ -13,7 +19,20 @@ const WorkspaceIdLayout = ({ children }: Props) => {
       <Toolbar />
       <div className="flex h-[calc(100vh-40px)]">
         <Sidebar />
-        {children}
+        <ResizablePanelGroup
+          direction="horizontal"
+          autoSave="aw-workspace-layout"
+        >
+          <ResizablePanel
+            defaultSize={20}
+            minSize={11}
+            className="bg-[#5E2C5F]"
+          >
+            <WorkspaceSidebar />
+          </ResizablePanel>
+          <ResizableHandle withHandle></ResizableHandle>
+          <ResizablePanel minSize={20}>{children}</ResizablePanel>
+        </ResizablePanelGroup>
       </div>
     </div>
   );
