c256cbf56eccacc90010b963f151aed7da3d5a25
awais512 <raza.awais25@gmail.com>
2024-12-01 19:47:11 +0000

    Styling message and timestamps

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { Doc, Id } from "../../convex/_generated/dataModel";
+import dynamic from "next/dynamic";
+import { format, isToday, isYesterday } from "date-fns";
+import { Hint } from "./hint";
+const Renderer = dynamic(() => import("@/components/renderer"), { ssr: false });
 
 interface Props {
   id: Id<"messages">;
@@ -26,6 +30,10 @@ interface Props {
   setEditingId: (id: Id<"messages"> | null) => void;
 }
 
+const formatFullTime = (date: Date) => {
+  return `${isToday(date) ? "Today" : isYesterday(date) ? "Yesterday" : format(date, "MMM d, yyyy")} at ${format(date, "h:mm:ss a")}`;
+};
+
 export const Message = ({
   id,
   memberId,
@@ -45,5 +53,16 @@ export const Message = ({
   threadImage,
   threadTimestamp,
 }: Props) => {
-  return <div>{JSON.stringify(body)}</div>;
+  return (
+    <div className="flex flex-col gap-2 p-1.5 px-5 hover:bg-gray-100/60 group relative">
+      <div className="flex items-start gap-2">
+        <Hint label={formatFullTime(new Date(createdAt))}>
+          <button className="text-xs text-muted-foreground opacity-0 group-hover:opacity-100 w-[40px] leading-[22px] text-center hover:underline">
+            {format(new Date(createdAt), "hh:mm")}
+          </button>
+        </Hint>
+      </div>
+      <Renderer value={body} />
+    </div>
+  );
 };
